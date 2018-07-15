2369f184d2677a3c23b9a7cf1b60ab9e7445755d
Dmytro Katyukha <firemage.dima@gmail.com>
2018-07-15 21:19:49 +0300

    Added .gitlab-ci.yml: to build images

---
+++ push_all.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname $(readlink -f $0));
+
+# Build images
+$SCRIPT_DIR/push.bash 7.0
+$SCRIPT_DIR/push.bash 8.0
+$SCRIPT_DIR/push.bash 9.0
+$SCRIPT_DIR/push.bash 10.0
+$SCRIPT_DIR/push.bash 11.0
+
+
